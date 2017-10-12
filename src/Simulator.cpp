1ca02650301a152a269bdd9e8ac6ad8eea305716
vsepulve <vsepulve@dcc.uchile.cl>
2017-10-12 17:39:12 -0300

    More events added to event list execution-

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -91,21 +91,168 @@ void Simulator::run(){
 }
 
 void Simulator::executeEvent(Event &event){
+
 	EventType type = event.getType();
 	const vector<double> num_params = event.getNumParams();
 	const vector<string> text_params = event.getTextParams();
+	
 	if(type == CREATE){
+		// nombre de la nueva poblacion
 		assert(text_params.size() == 1);
-		assert(num_params.size() == 1);
 		string name = text_params[0];
+		
+		// tamaño de la nueva poblacion
+		assert(num_params.size() == 1);
 		unsigned int size = static_cast<unsigned int>(num_params[0]);
+		
 		if( populations.find(name) != populations.end() ){
-			cerr<<"Simulator::executeEvent - Warning, already used population name ("<<name<<")\n";
+			cerr<<"Simulator::executeEvent - CREATE Warning, already used population name ("<<name<<").\n";
 			delete populations[name];
 		}
 		// Aqui tambien habria que indicar la especie u otras propiedades, quzias del pool
 		// En ese caso, quiza sea reazonable que el pool sea DE la poblacion
 		populations[name] = new Population(size);
+	}
+	else if(type == SPLIT){
+		// nombre del origen y de los 2 destinos
+		assert(text_params.size() == 3);
+		string src = text_params[0];
+		string dst1 = text_params[1];
+		string dst2 = text_params[2];
+		
+		// porcentaje del primer destino
+		assert(num_params.size() == 1);
+		double percentage = num_params[0];
+		assert(percentage => 0.0 && percentage <= 1.0);
+		unsigned int size1 = 0;
+		unsigned int size2 = 0;
+		
+		if( populations.find(src) == populations.end() ){
+			cerr<<"Simulator::executeEvent - SPLIT Warning, src population not found ("<<src<<"), omitting.\n";
+			return;
+		}
+		unsigned int size = populations[src]->size();
+		size1 = static_cast<unsigned int>(percentage * size);
+		size2 = size - size1;
+			
+		if( populations.find(dst1) != populations.end() ){
+			cerr<<"Simulator::executeEvent - SPLIT Warning, already used population name ("<<dst1<<").\n";
+			delete populations[dst1];
+		}
+		if( populations.find(dst2) != populations.end() ){
+			cerr<<"Simulator::executeEvent - SPLIT Warning, already used population name ("<<dst2<<").\n";
+			delete populations[dst2];
+		}
+		// Aqui tambien habria que indicar la especie u otras propiedades, quzias del pool
+		// En ese caso, quiza sea reazonable que el pool sea DE la poblacion
+		populations[dst1] = new Population(size1);
+		populations[dst2] = new Population(size2);
+	}
+	else if(type == MIGRATE){
+		// nombre del origen y del destino
+		assert(text_params.size() == 2);
+		string src = text_params[0];
+		string dst = text_params[1];
+		
+		// porcentaje del destino
+		assert(num_params.size() == 1);
+		double percentage = num_params[0];
+		assert(percentage => 0.0 && percentage <= 1.0);
+		
+		if( populations.find(src) == populations.end() ){
+			cerr<<"Simulator::executeEvent - MIGRATE Warning, src population not found ("<<src<<"), omitting.\n";
+			return;
+		}
+		unsigned int size = static_cast<unsigned int>(percentage * populations[src]->size());
+			
+		if( populations.find(dst) != populations.end() ){
+			cerr<<"Simulator::executeEvent - MIGRATE Warning, already used population name ("<<dst<<").\n";
+			delete populations[dst];
+		}
+		
+		// Aqui tambien habria que indicar la especie u otras propiedades, quzias del pool
+		// En ese caso, quiza sea reazonable que el pool sea DE la poblacion
+		populations[dst] = new Population(size);
+		
+		populations[src]->decrease(size);
+	}
+	else if(type == MERGE){
+		// nombre del origen 1 y 2, y del destino
+		assert(text_params.size() == 3);
+		string src1 = text_params[0];
+		string src2 = text_params[1];
+		string dst = text_params[2];
+		
+		if( populations.find(src1) == populations.end() ){
+			cerr<<"Simulator::executeEvent - MERGE Warning, src population not found ("<<src1<<"), omitting.\n";
+			return;
+		}
+		if( populations.find(src2) == populations.end() ){
+			cerr<<"Simulator::executeEvent - MERGE Warning, src population not found ("<<src2<<"), omitting.\n";
+			return;
+		}
+		
+		if( populations.find(dst) != populations.end() ){
+			cerr<<"Simulator::executeEvent - MERGE Warning, already used population name ("<<dst<<").\n";
+			delete populations[dst];
+		}
+		
+		// Aqui tambien habria que indicar la especie u otras propiedades, quzias del pool
+		// En ese caso, quiza sea reazonable que el pool sea DE la poblacion
+		populations[dst] = new Population();
+		populations[dst]->add( populations[src1] );
+		populations[dst]->add( populations[src2] );
+		
+		delete populations[src1];
+		delete populations[src2];
+	}
+	else if(type == INCREASE){
+		// nombre del origen
+		assert(text_params.size() == 1);
+		string src = text_params[0];
+		
+		// porcentaje del incremento
+		assert(num_params.size() == 1);
+		double percentage = num_params[0];
+		assert(percentage => 0.0);
+		
+		if( populations.find(src) == populations.end() ){
+			cerr<<"Simulator::executeEvent - INCREASE Warning, src population not found ("<<src<<"), omitting.\n";
+			return;
+		}
+		
+		unsigned int size = static_cast<unsigned int>(percentage * populations[src]->size());
+		populations[src]->increase( size );
+	}
+	else if(type == DECREASE){
+		// nombre del origen
+		assert(text_params.size() == 1);
+		string src = text_params[0];
+		
+		// porcentaje del decremento
+		assert(num_params.size() == 1);
+		double percentage = num_params[0];
+		assert(percentage => 0.0 && percentage <= 1.0);
+		
+		if( populations.find(src) == populations.end() ){
+			cerr<<"Simulator::executeEvent - DECREASE Warning, src population not found ("<<src<<"), omitting.\n";
+			return;
+		}
+		
+		unsigned int size = static_cast<unsigned int>(percentage * populations[src]->size());
+		populations[src]->decrease( size );
+	}
+	else if(type == EXTINCT){
+		// nombre del origen
+		assert(text_params.size() == 1);
+		string src = text_params[0];
+		
+		if( populations.find(src) == populations.end() ){
+			cerr<<"Simulator::executeEvent - EXTINCT Warning, src population not found ("<<src<<"), omitting.\n";
+			return;
+		}
+		
+		delete populations[src];
 	}
 	
 }
