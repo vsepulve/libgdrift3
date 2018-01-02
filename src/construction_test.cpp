294ce2cb9bea59bd562a4acc241b041b28d0ed60
vsepulve <vsepulve@dcc.uchile.cl>
2018-01-02 16:38:24 -0300

    Test program adjusted to create a factory, a simulator, and run it.

--- src/construction_test.cpp
+++ src/construction_test.cpp
@@ -5,6 +5,7 @@
 
 #include "NanoTimer.h"
 #include "SimulatorFactory.h"
+#include "Simulator.h"
 
 using namespace std;
 
@@ -19,20 +20,53 @@ int main(int argc,char** argv) {
 	
 	cout<<"Test - Inicio\n";
 	
+	cout<<"Test - Creando SimulatorFactory\n";
 	SimulatorFactory factory(json_file);
 	
 	NanoTimer timer;
-	unsigned int n = 100000;
-	unsigned int *arr = new unsigned int[n];
-	for(unsigned int i = 0; i < n; ++i){
-		for(unsigned int j = 0; j < n; ++j){
-			arr[j] = rand();
-		}
-	}
-	delete [] arr;
 	
+//	unsigned int n = 100000;
+//	unsigned int *arr = new unsigned int[n];
+//	for(unsigned int i = 0; i < n; ++i){
+//		for(unsigned int j = 0; j < n; ++j){
+//			arr[j] = rand();
+//		}
+//	}
+//	delete [] arr;
+	
+	cout<<"Test - Creando nuevo Simulator\n";
+	Simulator *sim = factory.getInstance();
+	
+	cout<<"Test - Corriendo Simulator\n";
+	sim->run();
+	
+	delete sim;
 	
 	cout<<"Test - Fin (tpo total: "<<timer.getMilisec()<<" ms)\n";
 
 	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
