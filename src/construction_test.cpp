9a5d43f2bf672fdf561f6e11b862d3ec2e7fbc3d
vsepulve <vsepulve@dcc.uchile.cl>
2018-01-04 17:56:14 -0300

    Profile test added.

--- src/construction_test.cpp
+++ src/construction_test.cpp
@@ -6,6 +6,7 @@
 #include "NanoTimer.h"
 #include "SimulatorFactory.h"
 #include "Simulator.h"
+#include "ProfileMarker.h"
 
 using namespace std;
 
@@ -33,6 +34,20 @@ int main(int argc,char** argv) {
 //		}
 //	}
 //	delete [] arr;
+	
+	cout<<"Test - Probando ProfileMarker\n";
+	MarkerType type = MARKER_SEQUENCE;
+	unsigned int len = 10;
+	MutationType mut_type = MUTATION_BASIC;
+	vector<double> params;
+	params.push_back(0.5);
+	params.push_back(20.1);
+	ProfileMarker marker_1(type, len, mut_type, params);
+	ProfileMarker marker_2 = marker_1;
+	cout<<"Marker 1: ";
+	marker_1.print();
+	cout<<"Marker 2: ";
+	marker_2.print();
 	
 	cout<<"Test - Creando nuevo Simulator\n";
 	Simulator *sim = factory.getInstance();
