8789c1c0b3f4f615d13883d490b9007d9a822fd9
vsepulve <vsepulve@dcc.uchile.cl>
2018-01-05 17:06:12 -0300

    Code to store the alleles information added.

--- include/Individual.h
+++ include/Individual.h
@@ -1,25 +1,32 @@
 #ifndef _INDIVIDUAL_H_
 #define _INDIVIDUAL_H_
 
+#include "Profile.h"
+
 using namespace std;
 
 class Individual{
 
 private:
+	vector<unsigned int> alleles;
 
 public:
 
 	Individual();
-	Individual(const Individual &original);
-	Individual& operator=(const Individual& original);
-	virtual Individual *clone();
+//	Individual(const Individual &original);
+//	Individual& operator=(const Individual& original);
+//	virtual Individual *clone();
 	virtual ~Individual();
 	
 	void setParent(Individual &parent);
 	
+	unsigned int getNumMarkers();
+	
 	unsigned int getAllele(unsigned int pos);
 	
 	void setAllele(unsigned int pos, unsigned int allele);
+	
+	void prepare(Profile *profile);
 	
 };
 
--- src/Individual.cpp
+++ src/Individual.cpp
@@ -3,19 +3,19 @@
 Individual::Individual(){
 }
 
-Individual::Individual(const Individual &original){
-}
+//Individual::Individual(const Individual &original){
+//}
 
-Individual& Individual::operator=(const Individual& original){
-	if (this != &original){
-	
-	}
-	return *this;
-}
+//Individual& Individual::operator=(const Individual& original){
+//	if (this != &original){
+//	
+//	}
+//	return *this;
+//}
 
-Individual *Individual::clone(){
-	return new Individual(*this);
-}
+//Individual *Individual::clone(){
+//	return new Individual(*this);
+//}
 
 Individual::~Individual(){
 	
@@ -24,12 +24,39 @@ Individual::~Individual(){
 void Individual::setParent(Individual &parent){
 	// Copiar alelos
 	// Model se encarga de la mutacion cuando sea necesario
+	alleles.resize(parent.getNumMarkers());
+	for(unsigned int i = 0; i < alleles.size(); ++i){
+		alleles[i] = parent.getAllele(i);
+	}
+}
+	
+unsigned int Individual::getNumMarkers(){
+	return alleles.size();
 }
 	
 unsigned int Individual::getAllele(unsigned int pos){
-	return 0;
+	return alleles[pos];
 }
 
 void Individual::setAllele(unsigned int pos, unsigned int allele){
-	
+	alleles[pos] = allele;
 }
+
+void Individual::prepare(Profile *profile){
+	alleles.clear();
+	alleles.resize(profile->getNumMarkers());
+	// Aqui podria ser necesario resetear OTROS datos dependiendo del tipo de sequencia quizas
+	// Quizas esa informacion pueda estar en el Pool, eso si, quizas si basta con el id del alelo
+}
+
+
+
+
+
+
+
+
+
+
+
+
