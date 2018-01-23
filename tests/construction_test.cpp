edd4e31b1131ea2ba9ed9130565cca04a4922880
vsepulve <vsepulve@dcc.uchile.cl>
2018-01-23 17:14:45 -0300

    Test changed to omit statistics and perform several tests (for memory testing).

--- tests/construction_test.cpp
+++ tests/construction_test.cpp
@@ -51,29 +51,44 @@ int main(int argc,char** argv) {
 	cout<<"Marker 2: ";
 	marker_2.print();
 	
-	cout<<"Test - Creando nuevo Simulator\n";
-	Simulator *sim = factory.getInstance();
+	unsigned int total_tests = 10000;
+	NanoTimer sim_timer;
+	cout<<"Test - Probando " << total_tests << " simulaciones\n";
+	cout<<"-----     -----\n";
 	
-	cout<<"Test - Corriendo Simulator\n";
-	sim->run();
+	for(unsigned int i = 0; i < total_tests; ++i){
+		cout<<"Test - Creando nuevo Simulator " << i << "\n";
+		sim_timer.reset();
+		Simulator *sim = factory.getInstance();
+		double factory_milisec = sim_timer.getMilisec();
 	
-	cout<<"Test - Verificando Estadisticos\n";
-	float sampling = 0.05;
-	Statistics stats(sim, sampling);
-	map<string, vector< map<string, double> > > statistics = stats.getStatistics();
-	for( auto &par : statistics ){
-		cout<<"Test - Stats Population \""<< par.first <<"\"\n";
-		unsigned int marker_pos = 0;
-		for( map<string, double> &marker_stats : par.second ){
-			cout<<"Test - [ Marker "<<marker_pos++<<" ]\n";
-			for( auto &stat : marker_stats ){
-				cout<<"Test - " << stat.first << ": "<< stat.second <<"\n";
-			}
-			cout<<"-----\n";
-		}
+		cout<<"Test - Corriendo Simulator\n";
+		sim_timer.reset();
+		sim->run();
+		double run_milisec = sim_timer.getMilisec();
+	
+	//	cout<<"Test - Verificando Estadisticos\n";
+	//	float sampling = 0.05;
+	//	Statistics stats(sim, sampling);
+	//	map<string, vector< map<string, double> > > statistics = stats.getStatistics();
+	//	for( auto &par : statistics ){
+	//		cout<<"Test - Stats Population \""<< par.first <<"\"\n";
+	//		unsigned int marker_pos = 0;
+	//		for( map<string, double> &marker_stats : par.second ){
+	//			cout<<"Test - [ Marker "<<marker_pos++<<" ]\n";
+	//			for( auto &stat : marker_stats ){
+	//				cout<<"Test - " << stat.first << ": "<< stat.second <<"\n";
+	//			}
+	//			cout<<"-----\n";
+	//		}
+	//	}
+	
+		delete sim;
+		cout<<"Test - factory_milisec: " << factory_milisec << "\n";
+		cout<<"Test - run_milisec: " << run_milisec << "\n";
+		cout<<"-----     -----\n";
 	}
 	
-	delete sim;
 	
 	cout<<"Test - Fin (tpo total: "<<timer.getMilisec()<<" ms)\n";
 
