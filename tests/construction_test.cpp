f1e843b69f6a4c30ad0c38cdb7227b2ac0701f57
vsepulve <vsepulve@dcc.uchile.cl>
2018-03-09 15:35:27 -0300

    Test for SimulatorFactory::reloadParameters added.

--- tests/construction_test.cpp
+++ tests/construction_test.cpp
@@ -36,21 +36,45 @@ int main(int argc,char** argv) {
 //		}
 //	}
 //	delete [] arr;
+
+	cout << "Test - Prueba de Reload\n";
+	SimulatorFactory test_factory(json_file);
+	Simulator *s1 = test_factory.getInstance();
+	s1->print();
 	
-	cout<<"Test - Probando ProfileMarker\n";
-	MarkerType type = MARKER_SEQUENCE;
-	unsigned int len = 10;
-	unsigned int init_alleles = 1000;
-	MutationType mut_type = MUTATION_BASIC;
-	vector<double> params;
-	params.push_back(0.5);
-	params.push_back(20.1);
-	ProfileMarker marker_1(type, len, init_alleles, mut_type, params);
-	ProfileMarker marker_2 = marker_1;
-	cout<<"Marker 1: ";
-	marker_1.print();
-	cout<<"Marker 2: ";
-	marker_2.print();
+	cout << "Test - Recargando\n";
+	// Por ahroa voy a OMITIR los parametros fixed
+	// Notar que esto implica no incluirlos si tienen stddev 0
+	// Quizas seria mejor un metodo mas seguro que ese
+	vector<pair<double, double>> new_params;
+	new_params.push_back(pair<double, double>(0.005, 0.001));
+	new_params.push_back(pair<double, double>(5000, 1000));
+	new_params.push_back(pair<double, double>(1000, 100));
+	new_params.push_back(pair<double, double>(0.2, 0.1));
+	new_params.push_back(pair<double, double>(2000, 100));
+	new_params.push_back(pair<double, double>(4000, 1000));
+	new_params.push_back(pair<double, double>(0.5, 0.1));
+	new_params.push_back(pair<double, double>(8000, 1000));
+	test_factory.reloadParameters(new_params);
+	
+	Simulator *s2 = test_factory.getInstance();
+	s2->print();
+	
+	
+//	cout<<"Test - Probando ProfileMarker\n";
+//	MarkerType type = MARKER_SEQUENCE;
+//	unsigned int len = 10;
+//	unsigned int init_alleles = 1000;
+//	MutationType mut_type = MUTATION_BASIC;
+//	vector<double> params;
+//	params.push_back(0.5);
+//	params.push_back(20.1);
+//	ProfileMarker marker_1(type, len, init_alleles, mut_type, params);
+//	ProfileMarker marker_2 = marker_1;
+//	cout<<"Marker 1: ";
+//	marker_1.print();
+//	cout<<"Marker 2: ";
+//	marker_2.print();
 	
 	NanoTimer sim_timer;
 	cout<<"Test - Probando " << total_tests << " simulaciones\n";
@@ -67,21 +91,21 @@ int main(int argc,char** argv) {
 		sim->run();
 		double run_milisec = sim_timer.getMilisec();
 	
-		cout<<"Test - Verificando Estadisticos\n";
-		float sampling = 0.05;
-		Statistics stats(sim, sampling);
-		map<string, vector< map<string, double> > > statistics = stats.getStatistics();
-		for( auto &par : statistics ){
-			cout<<"Test - Stats Population \""<< par.first <<"\"\n";
-			unsigned int marker_pos = 0;
-			for( map<string, double> &marker_stats : par.second ){
-				cout<<"Test - [ Marker "<<marker_pos++<<" ]\n";
-				for( auto &stat : marker_stats ){
-					cout<<"Test - " << stat.first << ": "<< stat.second <<"\n";
-				}
-				cout<<"-----\n";
-			}
-		}
+//		cout<<"Test - Verificando Estadisticos\n";
+//		float sampling = 0.05;
+//		Statistics stats(sim, sampling);
+//		map<string, vector< map<string, double> > > statistics = stats.getStatistics();
+//		for( auto &par : statistics ){
+//			cout<<"Test - Stats Population \""<< par.first <<"\"\n";
+//			unsigned int marker_pos = 0;
+//			for( map<string, double> &marker_stats : par.second ){
+//				cout<<"Test - [ Marker "<<marker_pos++<<" ]\n";
+//				for( auto &stat : marker_stats ){
+//					cout<<"Test - " << stat.first << ": "<< stat.second <<"\n";
+//				}
+//				cout<<"-----\n";
+//			}
+//		}
 	
 		delete sim;
 		cout<<"Test - factory_milisec: " << factory_milisec << "\n";
