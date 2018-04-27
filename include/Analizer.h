f99175abd48880b4dfd389557c0fca121946f789
vsepulve <vsepulve@dcc.uchile.cl>
2018-04-27 17:27:49 -0300

    First part of the analyzer code (using ResultReader) added.

--- include/Analizer.h
+++ include/Analizer.h
@@ -4,9 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <assert.h>
 
 #include <thread>
 #include <mutex>
+
+#include "WorkManager.h"
+#include "ResultsReader.h"
 
 using namespace std;
 
@@ -15,13 +19,21 @@ class Analizer{
 protected:
 
 private:
+	WorkManager *manager;
+	string file_base;
+	string target_base;
+	// Numero de threads de procesamiento
+	unsigned int n_threads;
 
 public:
 
 	Analizer();
+	Analizer(WorkManager *_manager, unsigned int _n_threads, string _file_base, string _target_base);
 	virtual ~Analizer();
 	
 	
+	
+	void execute(unsigned int sim_id);
 	
 	
 	
--- src/Analizer.cpp
+++ src/Analizer.cpp
@@ -1,11 +1,58 @@
 #include "Analizer.h"
 
 Analizer::Analizer(){
+	manager = NULL;
+	n_threads = 0;
+	file_base = "";
+}
+
+Analizer::Analizer(WorkManager *_manager, unsigned int _n_threads, string _file_base, string _target_base){
+	manager = _manager;
+	n_threads = _n_threads;
+	file_base = _file_base;
+	target_base = _target_base;
 }
 
 Analizer::~Analizer(){
+	// notar que el manager es del daemon
+	manager = NULL;
+}
+
+void Analizer::execute(unsigned int sim_id){
+
+	assert( manager != NULL);
+	assert( n_threads > 0);
+
+	// Leer los archivos de los threads de la simulacion
+	string sim_file_base = file_base;
+	sim_file_base += to_string(sim_id);
+	sim_file_base += "_";
+	
+	unsigned int n_stats = manager->getNumStatistics(sim_id);
+	unsigned int n_params = manager->getNumParams(sim_id);
+	
+	ResultsReader results;
+	results.readThreadsData(sim_file_base, n_threads, n_stats, n_params);
+	
+	// Potencialmente agrupar los archivos en un solo archivo consolidado (equivalente al de generate_statistics)
+	
+	// Cargar el target (o pedirlo al manager, aun no esta decidido)
+	string target_file = target_base;
+	target_file += to_string(sim_id);
+	results.setTarget(target_file);
+	
+	
 	
 }
+
+
+
+
+
+
+
+
+
 
 
 
