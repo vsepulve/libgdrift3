d7da5619f1ed404812ee385bdc1798ed7db2bdf1
vsepulve <vsepulve@dcc.uchile.cl>
2018-05-03 15:36:29 -0300

    Real implementation of table added.

--- include/StatusTable.h
+++ include/StatusTable.h
@@ -7,24 +7,57 @@
 
 #include <thread>
 #include <mutex>
+#include <map>
+
+#include "SimulatorFactory.h"
 
 using namespace std;
 
+// Este objeto es solo para agrupar la informacion de la simulacion
+// Solo esta pensado para StatusTable (notar que podria ser una inner class)
+class SimulationData{
+	public: 
+		SimulatorFactory *factory;
+		unsigned int n_sims;
+		unsigned int finished;
+		unsigned int feedback;
+		SimulationData(){
+			factory = NULL;
+			n_sims = 0;
+			finished = 0;
+			feedback = 0;
+		}
+		~SimulationData(){
+			factory = NULL;
+			n_sims = 0;
+			finished = 0;
+			feedback = 0;
+		}
+};
+
 class StatusTable{
 
-protected:
-
 private:
+	map<unsigned int, SimulationData> table;
 
 public:
 
 	StatusTable();
 	virtual ~StatusTable();
 	
+	void addSimulation(unsigned int sim_id, SimulatorFactory *factory, unsigned int n_sims);
 	
+	void addFinished(unsigned int sim_id);
 	
+	unsigned int getFinished(unsigned int sim_id);
 	
+	unsigned int getTotal(unsigned int sim_id);
 	
+	void addFeedback(unsigned int sim_id);
+	
+	unsigned int getFeedback(unsigned int sim_id);
+	
+	SimulatorFactory *getFactory(unsigned int sim_id);
 	
 };
 
--- src/StatusTable.cpp
+++ src/StatusTable.cpp
@@ -7,5 +7,34 @@ StatusTable::~StatusTable(){
 	
 }
 
+void StatusTable::addSimulation(unsigned int sim_id, SimulatorFactory *factory, unsigned int n_sims){	
+	table[sim_id].factory = factory;
+	table[sim_id].n_sims = n_sims;
+	table[sim_id].finished = 0;
+	table[sim_id].feedback = 0;
+}
 
+void StatusTable::addFinished(unsigned int sim_id){
+	table[sim_id].finished++;
+}
+
+unsigned int StatusTable::getFinished(unsigned int sim_id){
+	return table[sim_id].finished;
+}
+
+unsigned int StatusTable::getTotal(unsigned int sim_id){
+	return table[sim_id].n_sims;
+}
+
+void StatusTable::addFeedback(unsigned int sim_id){
+	table[sim_id].feedback++;
+}
+
+unsigned int StatusTable::getFeedback(unsigned int sim_id){
+	return table[sim_id].feedback;
+}
+
+SimulatorFactory *StatusTable::getFactory(unsigned int sim_id){
+	return table[sim_id].factory;
+}
 
